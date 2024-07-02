f37a9e63a3511fd258f3a589519a3401879458c7
NarakCODE <channarakluy@gmail.com>
2024-07-02 21:09:50 +0700

    Initial file

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="w-full h-screen grid place-content-center gap-4">
+      <AnimatedLink href="#">Instagram</AnimatedLink>
+    </div>
+  );
+};
+
+const AnimatedLink = ({ href, children }) => {
+  return (
+    <a
+      className="relative overflow-hidden border block whitespace-nowrap text-8xl font-semibold uppercase"
+      href={href}
+    >
+      <div>{children}</div>
+    </a>
+  );
+};
+
+export default App;
